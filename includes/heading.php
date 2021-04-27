1584a37093023d0e8678629b52e64ca888ed57fe
AmNetDT <amnetdt@yahoo.com>
2021-04-27 11:08:33 +0100

    updates

---
+++ includes/heading.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
+    <title>RILA</title>
+   
+    <link href="../src/assets/node_modules/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../src/assets/node_modules/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
+    
+    
+    <link href="../src/assets/node_modules/morrisjs/morris.css" rel="stylesheet">
+    
+    <link href="../src/assets/node_modules/c3-master/c3.min.css" rel="stylesheet">
+    
+    <link href="../src/css/style.css" rel="stylesheet">
+    
+    <link href="../src/css/pages/dashboard1.css" rel="stylesheet">
+    
+    <link href="../src/css/colors/default.css" id="theme" rel="stylesheet">
+   
+</head>
+
+<body class="fix-header fix-sidebar card-no-border">
+    
+    <div id="main-wrapper">
+        <header class="topbar shadow">
+            <nav class="navbar top-navbar navbar-expand-md navbar-light">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.html">
+                        
+                            <img src="../src/assets/images/logo.png" alt="homepage" class="dark-logo" />
+                            
+                        </b>
+                        
+                       <span class="font-weight-light" >R&nbsp;I&nbsp;L&nbsp;A</span>
+                    </a>
+                </div>
+                                <div class="navbar-collapse">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item"> 
+                            <a class="nav-link nav-toggler hidden-md-up waves-effect waves-dark" 
+                            href="javascript:void(0)">
+                            <i class="fa fa-bars"></i></a> </li>
+                    </ul>
+                    <ul class="navbar-nav my-lg-0">
+                       
+                        <li class="nav-item dropdown u-pro">
+                            <a class="nav-link dropdown-toggle waves-effect waves-dark profile-pic" href="" 
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img src="../assets/images/users/1.jpg" alt="user" class="" />
+                                <span class="hidden-md-down">Dosunmu AbdGaniu &nbsp;</span> </a>
+                        </li>
+                    </ul>
+                    
+
+                </div>
+            </nav>
+        </header>
