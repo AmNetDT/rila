291156c1e2f73299a87c54c4b11c17cd5a8bd2e2
AbdGaniu Dosunmu <amnetdt@yahoo.com>
2021-04-27 09:12:03 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Favicon icon -->
+  <link rel="icon" type="image/png" sizes="16x16" href="src/image/favicon.png" />
+  <title>RILA</title>
+  <link href="src/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="src/css/style.css" rel="stylesheet" />
+</head>
+<body>
+  <div class="container">
+    <div class="row justify-content-center py-5">
+         
+          <div class="col-sm-4 pt-3 border radius">
+            
+            <div class="card-body px-4 mb-5 text-center">
+              
+              <img src="src/image/logo.png" alt="logo" class="logo" />
+              <p class="login-card-description mt-2">
+                <a href="login/index">
+              Login to your account</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   
+    </div>
+        <footer class="footer fixed-bottom text-center text-dark ">
+            &copy; Redeemer's International Leadership Academy - RILA 
+            <script>document.write(new Date().getFullYear());</script> 
+        </footer>
+
+</body>
+</html>
